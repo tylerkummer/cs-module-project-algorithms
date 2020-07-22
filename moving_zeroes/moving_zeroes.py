cfc595cf3a91a486cb676718b942120dfcc2eb74
Tyler Kummer <tylerkummer@protonmail.com>
2020-07-22 16:50:53 -0500

    Moving Zeroes Fix

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -14,14 +14,8 @@ def moving_zeroes(arr):
     for i in range(len(arr)):
         # Check for non zero elements then replace the index of the element with count, and increment count at the end
         if arr[i] != 0:
-            arr[count] = arr[i]
+            arr[count], arr[i] = arr[i], arr[count]
             count += 1
-
-    # All Non zero elements are shifted to the front
-    while count < len(arr):
-        # Set arr[count] to index of zero and increment count, to move the elements 0 to the end
-        arr[count] = 0
-        count += 1
 
     return arr
 
