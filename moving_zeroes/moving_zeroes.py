6b3c5d1caae96b0a8a1f299ea5727eb9577bfa3d
Tyler Kummer <tylerkummer@protonmail.com>
2020-07-22 16:30:04 -0500

    Moving Zeroes Works

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,28 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    # Create a counter for non zero elements
+    count = 0
+
+    # Traverse the list
+    for i in range(len(arr)):
+        # Check for non zero elements then replace the index of the element with count, and increment count at the end
+        if arr[i] != 0:
+            arr[count] = arr[i]
+            count += 1
+
+    # All Non zero elements are shifted to the front
+    while count < len(arr):
+        # Set arr[count] to index of zero and increment count, to move the elements 0 to the end
+        arr[count] = 0
+        count += 1
+
+    return arr
 
 
 if __name__ == '__main__':
